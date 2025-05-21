83fc5d51ccc5788e7570b0fad9b2a72fb8228831
Hughes, Aaron John <ahughes9905@gmail.com>
2025-05-21 10:20:54 -0600

    added media upload

--- app/src/main/java/snowcoach/Service/InitDbService.java
+++ app/src/main/java/snowcoach/Service/InitDbService.java
@@ -1,94 +1,94 @@
-package snowcoach.Service;
-
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-import snowcoach.Model.Privilege;
-import snowcoach.Model.Role;
-import snowcoach.Model.User;
-import snowcoach.Repository.PrivilegeRepository;
-import snowcoach.Repository.RoleRepository;
-import snowcoach.Repository.UserRepository;
-
-
-import java.util.Arrays;
-
-@Service
-public class InitDbService {
-
-    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(10);
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private RoleRepository roleRepository;
-
-    @Autowired
-    private PrivilegeRepository privilegeRepository;
-
-    @PostConstruct
-    public void init() {
-        Privilege readPrivilege = new Privilege();
-        readPrivilege.setName("READ_PRIVILEGE");
-        Privilege writePrivilege = new Privilege();
-        writePrivilege.setName("WRITE_PRIVILEGE");
-        Privilege claimPrivilege = new Privilege();
-        claimPrivilege.setName("CLAIM_PRIVILEGE");
-
-        privilegeRepository.saveAll(Arrays.asList(readPrivilege, writePrivilege, claimPrivilege));
-
-        Role adminRole = new Role();
-        adminRole.setName("ROLE_ADMIN");
-        adminRole.setPrivileges(Arrays.asList(readPrivilege, writePrivilege));
-        roleRepository.save(adminRole);
-
-        Role userRole = new Role();
-        userRole.setName("ROLE_USER");
-        userRole.setPrivileges(Arrays.asList(readPrivilege, writePrivilege));
-        roleRepository.save(userRole);
-
-        Role coach1Role = new Role();
-        coach1Role.setName("ROLE_COACH1");
-        coach1Role.setPrivileges(Arrays.asList(readPrivilege, writePrivilege, claimPrivilege));
-        roleRepository.save(coach1Role);
-
-        Role coach2Role = new Role();
-        coach2Role.setName("ROLE_COACH2");
-        coach2Role.setPrivileges(Arrays.asList(readPrivilege, writePrivilege, claimPrivilege));
-        roleRepository.save(coach2Role);
-
-        Role coach3Role = new Role();
-        coach3Role.setName("ROLE_COACH3");
-        coach3Role.setPrivileges(Arrays.asList(readPrivilege, writePrivilege, claimPrivilege));
-        roleRepository.save(coach3Role);
-
-
-        User coach2 = new User();
-        coach2.setUsername("coach2");
-        coach2.setPassword(encoder.encode("pass123"));
-        coach2.setRoles(Arrays.asList(coach1Role, coach2Role));
-        userRepository.save(coach2);
-
-        User coach3 = new User();
-        coach3.setUsername("coach3");
-        coach3.setPassword(encoder.encode("pass123"));
-        coach3.setRoles(Arrays.asList(coach1Role, coach2Role, coach3Role));
-        userRepository.save(coach3);
-
-        User athlete = new User();
-        athlete.setUsername("athlete");
-        athlete.setPassword(encoder.encode("pass123"));
-        athlete.setRoles(Arrays.asList(userRole));
-        userRepository.save(athlete);
-
-
-        User admin = new User();
-        admin.setUsername("admin");
-        admin.setPassword(encoder.encode("admin123"));
-        admin.setRoles(Arrays.asList(adminRole, coach1Role, coach2Role, coach3Role));
-        userRepository.save(admin);
-    }
-}
+//package snowcoach.Service;
+//
+//import jakarta.annotation.PostConstruct;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Service;
+//import snowcoach.Model.Privilege;
+//import snowcoach.Model.Role;
+//import snowcoach.Model.User;
+//import snowcoach.Repository.PrivilegeRepository;
+//import snowcoach.Repository.RoleRepository;
+//import snowcoach.Repository.UserRepository;
+//
+//
+//import java.util.Arrays;
+//
+//@Service
+//public class InitDbService {
+//
+//    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(10);
+//
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @Autowired
+//    private RoleRepository roleRepository;
+//
+//    @Autowired
+//    private PrivilegeRepository privilegeRepository;
+//
+//    @PostConstruct
+//    public void init() {
+//        Privilege readPrivilege = new Privilege();
+//        readPrivilege.setName("READ_PRIVILEGE");
+//        Privilege writePrivilege = new Privilege();
+//        writePrivilege.setName("WRITE_PRIVILEGE");
+//        Privilege claimPrivilege = new Privilege();
+//        claimPrivilege.setName("CLAIM_PRIVILEGE");
+//
+//        privilegeRepository.saveAll(Arrays.asList(readPrivilege, writePrivilege, claimPrivilege));
+//
+//        Role adminRole = new Role();
+//        adminRole.setName("ROLE_ADMIN");
+//        adminRole.setPrivileges(Arrays.asList(readPrivilege, writePrivilege));
+//        roleRepository.save(adminRole);
+//
+//        Role userRole = new Role();
+//        userRole.setName("ROLE_USER");
+//        userRole.setPrivileges(Arrays.asList(readPrivilege, writePrivilege));
+//        roleRepository.save(userRole);
+//
+//        Role coach1Role = new Role();
+//        coach1Role.setName("ROLE_COACH1");
+//        coach1Role.setPrivileges(Arrays.asList(readPrivilege, writePrivilege, claimPrivilege));
+//        roleRepository.save(coach1Role);
+//
+//        Role coach2Role = new Role();
+//        coach2Role.setName("ROLE_COACH2");
+//        coach2Role.setPrivileges(Arrays.asList(readPrivilege, writePrivilege, claimPrivilege));
+//        roleRepository.save(coach2Role);
+//
+//        Role coach3Role = new Role();
+//        coach3Role.setName("ROLE_COACH3");
+//        coach3Role.setPrivileges(Arrays.asList(readPrivilege, writePrivilege, claimPrivilege));
+//        roleRepository.save(coach3Role);
+//
+//
+//        User coach2 = new User();
+//        coach2.setUsername("coach2");
+//        coach2.setPassword(encoder.encode("pass123"));
+//        coach2.setRoles(Arrays.asList(coach1Role, coach2Role));
+//        userRepository.save(coach2);
+//
+//        User coach3 = new User();
+//        coach3.setUsername("coach3");
+//        coach3.setPassword(encoder.encode("pass123"));
+//        coach3.setRoles(Arrays.asList(coach1Role, coach2Role, coach3Role));
+//        userRepository.save(coach3);
+//
+//        User athlete = new User();
+//        athlete.setUsername("athlete");
+//        athlete.setPassword(encoder.encode("pass123"));
+//        athlete.setRoles(Arrays.asList(userRole));
+//        userRepository.save(athlete);
+//
+//
+//        User admin = new User();
+//        admin.setUsername("admin");
+//        admin.setPassword(encoder.encode("admin123"));
+//        admin.setRoles(Arrays.asList(adminRole, coach1Role, coach2Role, coach3Role));
+//        userRepository.save(admin);
+//    }
+//}
--- frontend/snowcoach-frontend/src/pages/CreatePost.tsx
+++ frontend/snowcoach-frontend/src/pages/CreatePost.tsx
@@ -2,14 +2,15 @@ import React, { useState } from "react";
 import { useAuth } from "../context/AuthContext";
 
 function CreatePost() {
-    const { user } = useAuth(); // Access user info from context
+    const { user } = useAuth();
     const [formData, setFormData] = useState({
         title: "",
         level: "",
         topic: "",
         body: "",
-        username: user?.username || "", 
+        username: user?.username || "",
     });
+    const [media, setMedia] = useState<File | null>(null);
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
         const { name, value } = e.target;
@@ -19,22 +20,56 @@ function CreatePost() {
         }));
     };
 
+    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files && e.target.files.length > 0) {
+            setMedia(e.target.files[0]);
+        } else {
+            setMedia(null);
+        }
+    };
+
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
 
         try {
-            console.log("data sent:", formData.username);
-            const response = await fetch("http://localhost:8080/api/posts/create", {
-                method: "POST",
-                credentials: "include",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    ...formData,
-                }),
-            });
-            console.log("FormData:", formData);
+            let response;
+            if (media) {
+                const data = new FormData();
+
+                // Properly send post JSON with content-type
+                data.append(
+                "post",
+                new Blob(
+                    [JSON.stringify({
+                    title: formData.title,
+                    level: formData.level,
+                    topic: formData.topic,
+                    body: formData.body,
+                    username: formData.username,
+                    })],
+                    { type: "application/json" }
+                )
+                );
+                data.append("file", media);
+
+                response = await fetch("http://localhost:8080/api/posts/create/media", {
+                    method: "POST",
+                    credentials: "include",
+                    body: data,
+                });
+            } else {
+                // Use JSON for regular post
+                response = await fetch("http://localhost:8080/api/posts/create", {
+                    method: "POST",
+                    credentials: "include",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        ...formData,
+                    }),
+                });
+            }
 
             if (!response.ok) {
                 throw new Error("Failed to create post");
@@ -46,6 +81,7 @@ function CreatePost() {
 
             // Reset the form
             setFormData({ title: "", level: "", topic: "", body: "", username: user?.username || "" });
+            setMedia(null);
         } catch (error) {
             console.error("Error creating post:", error);
             alert("Failed to create post. Please try again.");
@@ -102,6 +138,16 @@ function CreatePost() {
                         value={formData.body}
                         onChange={handleChange}
                         required
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="media">Media (photo or video):</label>
+                    <input
+                        type="file"
+                        id="media"
+                        name="media"
+                        accept="image/*,video/*"
+                        onChange={handleFileChange}
                     />
                 </div>
                 <button type="submit" className="submit-btn">Create Post</button>
