b06d156a39cf64f22374a90e85c9e322ccdfd32d
Hughes, Aaron John <ahughes9905@gmail.com>
2025-06-12 10:27:14 -0600

    merged single nginx into adev

--- backend/app/src/main/java/snowcoach/Config/WebConfig.java
+++ backend/app/src/main/java/snowcoach/Config/WebConfig.java
@@ -16,12 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins(
-//                            "http://localhost:3000",
-//                            "http://frontend:3000",
-                            "http://localhost:80",
-                            "https://snowcoach.pro"
-                        )
+                        .allowedOrigins("http://localhost:3000", "http://frontend:3000", "http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
